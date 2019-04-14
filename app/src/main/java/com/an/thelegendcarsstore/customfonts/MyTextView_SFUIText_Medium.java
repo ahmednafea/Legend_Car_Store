9c1beacb5e1acf0e6c0830d161a96cf4aabb1750
Ahmed Nafea Mohamed <32443261+ahmednafea@users.noreply.github.com>
2019-04-15 01:51:02 +0200

    Models Update

---
+++ app/src/main/java/com/an/thelegendcarsstore/customfonts/MyTextView_SFUIText_Medium.java
@@ -0,0 +1,32 @@
+package com.an.thelegendcarsstore.customfonts;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+
+public class MyTextView_SFUIText_Medium extends android.support.v7.widget.AppCompatTextView {
+
+    public MyTextView_SFUIText_Medium(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public MyTextView_SFUIText_Medium(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyTextView_SFUIText_Medium(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/SFUIText-Medium.ttf.ttf");
+            setTypeface(tf);
+        }
+    }
+
+}
